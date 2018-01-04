640e883f361e30bfc047fe2f04bd190d1c24d96d
horbatiuk88 <horbatiuk88@gmail.com>
2018-01-04 09:07:11 +0200

    creating project with Spring Initializr

---
+++ src/main/java/com/horbatiuk88/restaurantvoter/RestaurantVoterApplication.java
@@ -0,0 +1,12 @@
+package com.horbatiuk88.restaurantvoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantVoterApplication.class, args);
+	}
+}
---
+++ src/main/java/com/horbatiuk88/restaurantvoter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.horbatiuk88.restaurantvoter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestaurantVoterApplication.class);
+	}
+
+}
